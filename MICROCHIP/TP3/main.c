5cfb0d9534c2348d6f33e7b99d66efa528877095
Nacho <inifl99@gmail.com>
2024-06-06 18:37:04 -0300

    project init

---
+++ MICROCHIP/TP3/main.c
@@ -0,0 +1,18 @@
+/*
+ * TP3.c
+ *
+ * Created: 6/6/2024 18:34:12
+ * Author : Ignacio
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
