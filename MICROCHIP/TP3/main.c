e50b5b1786bb57588ee4ded95b01ad398ab8f315
Ignacio Flores <ignacio.flores@snappler.com>
2024-06-08 14:21:34 -0300

    codigo del timer, uart, y RTC y diseño proteus

--- MICROCHIP/TP3/main.c
+++ MICROCHIP/TP3/main.c
@@ -3,41 +3,43 @@
  *
  * Created: 6/6/2024 18:34:12
  * Author : Ignacio
- */ 
+ */
+#include "utils.h"
 #include "rtc.h"
 #include <stdio.h>
 #include "uart.h"
 #include "timer.h"
 #include "cli.h"
 
-int main(void) {
+int main(void)
+{
 	UART_Init(); // Inicializa el UART
-	rtc_init(); // Inicializa el RTC
+	rtc_init();	 // Inicializa el RTC
 	TIMER_init();
-	CLI_Init();
+	TIMER_Enable();
+	// CLI_Init();
 	RTC_Time now;
-	
-	sei();
 
-	while (1) {
-		
+	sei();
+	while (1)
+	{
+		/*
 		if (UART_GetCmdFlag()) // Recibo un comando
 		{
 			UART_ClearCmdFlag();
 			CLI_cmd(UART_ReadBuffer());
 			UART_Enable_Rx();
-		}
+		}*/
 
-		if (CLI_GetCommandFlag() && TIMER_GetClkFlag())
+		if (TIMER_GetClkFlag())
 		{
 			TIMER_ClearClkFlag();
 			now = rtc_get_time();
 			char buffer[30]; // Buffer para almacenar la cadena de tiempo
 			sprintf(buffer, "Hora actual: %02d:%02d:%02d\r\n", now.hours, now.minutes, now.seconds);
-			UART_SendMsg(buffer); // Envía la cadena por UART
+			UART_SendMsg(buffer); // Envï¿½a la cadena por UART
 		}
 	}
 
 	return 0;
 }
-
